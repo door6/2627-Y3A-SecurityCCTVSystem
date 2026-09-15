5e8e100af3d91f8758c84700cd359a4c3065b3c2
door6 <122876190+door6@users.noreply.github.com>
2026-09-15 10:55:48 +0200

    Implement startup and shutdown functions for the security system module

--- SecuritySystemDemo/Plugins/SecurityCCTVSystem/Source/SecurityCCTVSystem/Private/SecurityCCTVSystem.cpp
+++ SecuritySystemDemo/Plugins/SecurityCCTVSystem/Source/SecurityCCTVSystem/Private/SecurityCCTVSystem.cpp
@@ -2,17 +2,36 @@
 
 #include "SecurityCCTVSystem.h"
 
+#include "PropertyEditorModule.h"
+#include "SecurityManager.h"
+#include "SecurityManagerInterface.h"
+#include "Modules/ModuleManager.h"
+
 #define LOCTEXT_NAMESPACE "FSecurityCCTVSystemModule"
 
 void FSecurityCCTVSystemModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+
+	FPropertyEditorModule& PropertyModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
+
+	PropertyModule.RegisterCustomClassLayout(
+		ASecurityManager::StaticClass()->GetFName(),
+		FOnGetDetailCustomizationInstance::CreateStatic(&FSecurityManagerInterface::MakeInstance)
+	);
 }
 
 void FSecurityCCTVSystemModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+
+	if (FModuleManager::Get().IsModuleLoaded("PropertyEditor"))
+	{
+		FPropertyEditorModule& PropertyModule =
+			FModuleManager::GetModuleChecked<FPropertyEditorModule>("PropertyEditor");
+		PropertyModule.UnregisterCustomClassLayout(ASecurityManager::StaticClass()->GetFName());
+	}
 }
 
 #undef LOCTEXT_NAMESPACE
